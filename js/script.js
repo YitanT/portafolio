f9be1c3fd09d03272ccca9a0fe1fc73386ffd667
yitan <yitanangelo@hotmail.com>
2025-11-10 21:23:58 -0400

    terminando

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@ const toggleTheme = document.getElementById("toggle-theme");
 
 const toggleIcon = document.getElementById("toggle-icon");
 
-const toggleText = document.getElementById("toggle-text");
-
-
 const rootStyles = document.documentElement.style;
 
 
